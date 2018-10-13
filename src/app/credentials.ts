04b6023d931808580ddc052fee59f45eb4f9a87d
kaiser_jepyoo <danielgaldicanpro.com>
2018-10-14 05:34:24 +0800

    First commit

---
+++ src/app/credentials.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD96kgHXpqNOraIRcoSjDrZD8JF6p2rQ8g",
+    authDomain: "ggdiary-d2de8.firebaseapp.com",
+    databaseURL: "https://ggdiary-d2de8.firebaseio.com",
+    projectId: "ggdiary-d2de8",
+    storageBucket: "ggdiary-d2de8.appspot.com",
+    messagingSenderId: "795587079082"
+};
